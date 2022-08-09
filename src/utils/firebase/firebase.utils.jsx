90f12d226c4f987319f8a6d1c057ff1736e245eb
Tahseen <bhat.tahseen8@gmail.com>
2022-08-09 15:18:10 +0530

    created sign-in componet, integrated firebase authentication and database in our project

---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,62 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvZWjeVjE5akAF_wlpt3xZm_xEYOppk7A",
+  authDomain: "your-clothing-db-2d5d2.firebaseapp.com",
+  projectId: "your-clothing-db-2d5d2",
+  storageBucket: "your-clothing-db-2d5d2.appspot.com",
+  messagingSenderId: "685239821923",
+  appId: "1:685239821923:web:0174b2263f247cf85d3a56",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  // if user data does not exist
+  // create/set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  // if user data does exist! return userDocRef
+  return userDocRef;
+};
