67b8e6593c57e6d936e5ccfa24bf587454c7c7d8
Tahseen <bhat.tahseen8@gmail.com>
2022-08-12 17:11:19 +0530

    Created sing-up, input-form and button components

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,20 @@
+import "./button.styles.scss";
+
+// This is to apply different style classes to the button element
+const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
