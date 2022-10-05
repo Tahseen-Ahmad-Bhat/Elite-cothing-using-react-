8682efeb2fdba1067d66c05f1da50d65acd621e2
Tahseen <bhat.tahseen8@gmail.com>
2022-10-05 20:40:24 +0530

    added serviceWorker file to make our web as PWA

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import { store, persistor } from "./store/store";
 import { stripePromise } from "./utils/stripe/stripe.utils";
-
-// import * as serviceWorker from "./service-worker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import "./index.scss";
 
@@ -29,7 +28,10 @@ root.render(
   </React.StrictMode>
 );
 
-// serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
